6900d49d441df90679cdd4ba76b266331d98e7be
Pengnan Chi <47472606+pengnanchi@users.noreply.github.com>
2022-04-02 03:36:35 +0800

    Update dataset.py
    Update Torchaudio.info

--- cpc/dataset.py
+++ cpc/dataset.py
@@ -410,8 +410,17 @@ class SameSpeakerSampler(Sampler):
 
 def extractLength(couple):
     speaker, locPath = couple
-    info = torchaudio.info(str(locPath))[0]
-    return info.length
+    
+    #info = torchaudio.info(str(locPath))[0]
+    #return info.length
+
+    try:
+        info = torchaudio.info(str(locPath))[0]
+        dur = info.length
+    except:
+        info = torchaudio.info(str(locPath))
+        dur = info.num_frames
+    return dur
 
 
 def findAllSeqs(dirName,
